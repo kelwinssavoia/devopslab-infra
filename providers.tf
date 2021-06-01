52a179df5bf1094f8f9f08ad90d8b8d2b707e0e1
Kelwin Savoia <kelwinssavoia@gmail.com>
2021-05-31 23:16:38 -0300

    Terraform GCP Basics

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.64.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/vagrant/mystical-magnet-315402-8f518299c260.json")
+
+  project = "mystical-magnet-315402"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
