78e94aae16493683195f05b9511614782906c101
Kelwin Savoia <kelwinssavoia@gmail.com>
2021-05-31 23:43:31 -0300

    Convertendo Crendtial em Variável

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/vagrant/mystical-magnet-315402-8f518299c260.json")
-
   project = "mystical-magnet-315402"
   region  = "us-central1"
   zone    = "us-central1-c"
